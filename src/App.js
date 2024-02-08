94230d6e3577cd8ec0f44157ca4996a652b64e0d
Avinash Chavan <avinashchavan@Avinashs-MacBook-Air.local>
2024-02-08 17:07:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import platform from "platform";
 
 function App() {
+  console.log("Platform", platform);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Your OS Details </h1>
+      <h3>{"Tostring: " + platform.toString()}</h3>
+      <h3>{"OS: " + platform.os.toString()}</h3>
+      <h3>{"Browser: " + platform.name}</h3>
+      <h3>{"Version: " + platform.version}</h3>
+      <h3>{"Description: " + platform.description}</h3>
+      <h3>{"Layout: " + platform.layout}</h3>
+      <h3>{"Product: " + platform.product}</h3>
+      <h3>{"Manufacturer: " + platform.manufacturer}</h3>
+      <h3>{"Kernal: " + platform.kernal}</h3>
+      <h3>{"ua: " + platform.ua}</h3>
     </div>
   );
 }
